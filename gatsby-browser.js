d2cc5f310e174e34eab590b2199f839876532d3d
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-01-25 01:38:35 +0500

    Update gatsby-browser.js

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-// import store from "./src/store/userSlice.js"
-// import {Provider} from "react-redux"
-
-// export const wrapRootElement = ({element}) => {
-//         return  (
-//             <Provider store = {store} >{element}</Provider>
-
-//         )
-// }
